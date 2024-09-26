38ab9bbc7d674445992952e8fc69341bc44f0197
Zakir <zaakiraza110@gmail.com>
2024-09-26 00:35:01 -0700

    updates

---
+++ app.js
@@ -0,0 +1,23 @@
+fun=(i)=>{
+    document.getElementById(`box${i}`).style.backgroundColor="#B9FF65";
+    document.getElementById(`ending${i}`).style.display="block";
+    document.getElementById(`plus${i}`).style.display="none";
+    document.getElementById(`minus${i}`).style.display="flex";
+}
+
+fun1=(j)=>{
+    document.getElementById(`box${j}`).style.backgroundColor="#F3F3F3";
+    document.getElementById(`ending${j}`).style.display="none";
+    document.getElementById(`plus${j}`).style.display="flex";
+    document.getElementById(`minus${j}`).style.display="none";
+}
+openmenu=()=>{
+    document.getElementById('nav_items2').style.display="flex";
+    document.getElementById('menu').style.display="none"
+    document.getElementById('cross').style.display="flex";
+}
+closemenu=()=>{
+    document.getElementById('nav_items2').style.display="none";
+    document.getElementById('menu').style.display="flex"
+    document.getElementById('cross').style.display="none";
+}
